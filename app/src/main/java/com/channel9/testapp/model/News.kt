c9959fcea9e8d8c5c7aa057c3ffbf014c6169bba
sam911 <enamul.haque.sumon>
2023-04-17 10:42:19 +0800

    Added News model data class

---
+++ app/src/main/java/com/channel9/testapp/model/News.kt
@@ -0,0 +1,11 @@
+package com.channel9.testapp.model
+
+data class News(
+    val id: Long,
+    val url: String,
+    val headline: String,
+    val theAbstract: String,
+    val byLine: String,
+    val thumbnailUrl: String,
+    val timeStamp: Long
+)
