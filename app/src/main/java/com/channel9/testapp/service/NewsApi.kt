01e4a64bdc7c1e2cd521ce216bbc992745582d91
sam911 <enamul.haque.sumon>
2023-04-17 12:12:57 +0800

    Added NewsApi

---
+++ app/src/main/java/com/channel9/testapp/service/NewsApi.kt
@@ -0,0 +1,10 @@
+package com.channel9.testapp.service
+
+import com.channel9.testapp.service.model.NewsListResponse
+import retrofit2.Response
+import retrofit2.http.GET
+
+interface NewsApi {
+    @GET("1/coding_test/13ZZQX/full/")
+    suspend fun getNewsList(): Response<NewsListResponse>
+}
