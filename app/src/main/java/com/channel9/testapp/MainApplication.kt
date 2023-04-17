42a28a362067d8e9a63abd76be00269869c9c486
sam911 <enamul.haque.sumon>
2023-04-17 11:07:23 +0800

    Added Retrofit2

--- app/src/main/java/com/channel9/testapp/MainApplication.kt
+++ app/src/main/java/com/channel9/testapp/MainApplication.kt
@@ -5,6 +5,7 @@ import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
 import org.koin.dsl.module
+import retrofit2.Retrofit
 
 class MainApplication : Application() {
 
@@ -22,5 +23,9 @@ class MainApplication : Application() {
 }
 
 val appModule = module {
-
+    single {
+        Retrofit.Builder()
+            .baseUrl("https://bruce-v2-mob.fairfaxmedia.com.au/1/coding_test/13ZZQX/full")
+            .build()
+    }
 }
