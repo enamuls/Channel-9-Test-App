6b00db0cf684e83ff4a2bbc0947ede3aa1152d52
sam911 <enamul.haque.sumon>
2023-04-17 13:50:53 +0800

    Refactored adapter to use listAdapter

--- app/src/main/java/com/channel9/testapp/adapter/NewsListAdapter.kt
+++ app/src/main/java/com/channel9/testapp/adapter/NewsListAdapter.kt
@@ -2,15 +2,15 @@ package com.channel9.testapp.adapter
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import com.channel9.testapp.databinding.NewsItemViewBinding
+import com.channel9.testapp.databinding.LayoutNewsItemBinding
 import com.channel9.testapp.model.News
 
-class NewsListAdapter : RecyclerView.Adapter<NewsListAdapter.NewsListViewHolder>() {
-    private val newsList: List<News> = emptyList()
-
+class NewsListAdapter : ListAdapter<News, NewsListAdapter.NewsListViewHolder>(NewsDiffCallBack()) {
     class NewsListViewHolder(
-        private val binding: NewsItemViewBinding
+        private val binding: LayoutNewsItemBinding
     ) : RecyclerView.ViewHolder(binding.root) {
         fun bind(news: News) {
             binding.news = news
@@ -20,14 +20,22 @@ class NewsListAdapter : RecyclerView.Adapter<NewsListAdapter.NewsListViewHolder>
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NewsListViewHolder {
         val inflater = LayoutInflater.from(parent.context)
-        val binding = NewsItemViewBinding.inflate(inflater, parent, false)
+        val binding = LayoutNewsItemBinding.inflate(inflater, parent, false)
         return NewsListViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: NewsListViewHolder, position: Int) {
-        val news = newsList[position]
+        val news = getItem(position)
         holder.bind(news)
     }
 
-    override fun getItemCount(): Int = newsList.size
+    private class NewsDiffCallBack : DiffUtil.ItemCallback<News>() {
+        override fun areItemsTheSame(oldItem: News, newItem: News): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(oldItem: News, newItem: News): Boolean {
+            return oldItem == newItem
+        }
+    }
 }
